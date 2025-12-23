4f961a1dabf793314a398c189bdf288b6bdabd9a
Eva van Heese <eva.vanheese@quicknet.nl>
2025-12-23 14:18:21 +0100

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 export const profile = {
-	fullName: 'Your Name',
+	fullName: 'Eva van Heese',
 	title: '',
 	institute: '',
 	author_name: '', // Author name to be highlighted in the papers section
@@ -23,14 +23,14 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
-	menu_left: false,
-	transitions: true,
-	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
-	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
-	excerptLength: 200,
-	postPerPage: 5,
-    base: '' // Repository name starting with /
+  website_url: 'https://evavanheese.github.io',
+  menu_left: false,
+  transitions: true,
+  lightTheme: 'light',
+  darkTheme: 'dark',
+  excerptLength: 200,
+  postPerPage: 5,
+  base: '' // user site: keep empty
 }
 
 export const seo = {
