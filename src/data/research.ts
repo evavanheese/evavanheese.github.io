5362a5fe51e9aba73d88a95c55baf07a7d95dc10
Eva van Heese <eva.vanheese@quicknet.nl>
2026-04-27 23:10:11 +0200

    Add files via upload

---
+++ src/data/research.ts
@@ -0,0 +1,57 @@
+export const consortia = [
+  {
+    name: 'ENIGMA-PD',
+    url: 'https://enigma.ini.usc.edu/ongoing-studies/enigma-parkinsons/',
+    description: 'Worldwide consortium pooling neuroimaging data to study brain changes in Parkinson\'s disease.',
+    role: 'Site coordinator, data analyst, open science lead',
+  },
+  {
+    name: 'NICHY',
+    url: 'https://nichy.org/',
+    description: 'Global multi-site neuroimaging consortium focused on central disorders of hypersomnolence.',
+    role: 'Co-founder, site coordinator, MRI processing lead',
+  },
+  {
+    name: 'Dutch Society for Sleep-Wake Research (NSWO)',
+    url: 'https://www.nswo.nl/en/',
+    description: 'National society promoting sleep research and clinical practice in the Netherlands.',
+    role: 'Early Career Researchers Committee Member (2023-2026)',
+  },
+  {
+    name: 'Organization for Human Brain Mapping (OHBM)',
+    url: 'https://humanbrainmapping.org/',
+    description: 'International organisation dedicated to the use of neuroimaging to discover the organisation of the human brain.',
+    role: 'Secretary, Open Science Special Interest Group (2024-present)',
+  },
+];
+
+export const currentProjects = [
+  {
+    name: 'REMIND',
+    subtitle: 'Perivascular spaces and sleep in Lewy body disease',
+    status: 'Data collected, preregistration being drafted',
+    statusLink: '',
+    percentage: 40,
+  },
+  {
+    name: 'NICHY',
+    subtitle: 'Brain morphology in central disorders of hypersomnolence',
+    status: 'Preregistration available, guiding collaborating sites with MRI processing',
+    statusLink: 'https://osf.io/8fdqc',
+    percentage: 60,
+  },
+  {
+    name: 'ENIGMA-PD',
+    subtitle: 'Neuropsychiatry and brain morphology in Parkinson\'s disease',
+    status: 'Currently receiving data from sites!',
+    statusLink: '',
+    percentage: 75,
+  },
+  {
+    name: 'Hypersomnia Nap Study',
+    subtitle: 'Neurovascular coupling during sleep stages (including NREM3 and REM!)',
+    status: 'Preregistration available, first analyses done, further analysis ideas being brainstormed!',
+    statusLink: 'https://osf.io/492vs',
+    percentage: 80,
+  },
+];
