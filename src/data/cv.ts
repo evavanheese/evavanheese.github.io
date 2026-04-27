b8c436ea5612c4f813f8ff623ddf8e0dfc6038e1
Eva van Heese <eva.vanheese@quicknet.nl>
2026-04-27 10:58:54 +0200

    Update cv.ts

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,63 +1,159 @@
-export const experiences = [
-	{
-		company: '',
-		time: '',
-		title: '',
-		location: '',
-		description: '',
-	},
-	// {
-	// 	company: 'Radium Institute (Institut du Radium)',
-	// 	time: '1914 - 1934',
-	// 	title: 'Director',
-	// 	location: 'Paris, France',
-	// 	description: 'Led groundbreaking studies on radioactivity and mentored future Nobel Prize laureates.',
-	// },
-];
-
 export const education = [
-	{
-		school: '',
-		time: '',
-		degree: '',
-		location: '',
-		description: '',
-	},
-	// {
-	// 	school: 'University of Paris',
-	// 	time: '1891 - 1895',
-	// 	degree: 'Master’s in Physics and Mathematics',
-	// 	location: 'Paris, France',
-	// 	description: 'Graduated at the top of her class in physics and second in mathematics.',
-	// },
+  {
+    school: 'Amsterdam UMC & Leiden UMC',
+    time: '2023 - present',
+    degree: 'PhD in Neurosciences',
+    location: 'Amsterdam & Leiden, NL',
+    description: 'Dept. of Anatomy and Neurosciences, Amsterdam UMC, and C.J. Gorter MRI Center, Dept. of Radiology, Leiden UMC. Supervisors: Ysbrand van der Werf, Odile van den Heuvel, Thanh Dang-Vu, Jari Gool. Research focus: sleep and sleep disorders (central disorders of hypersomnolence, REM sleep behaviour disorder, brain clearance), neurodegeneration (Parkinson\'s disease, Lewy Body Dementia), advanced MRI analysis, international consortia, and open science.',
+  },
+  {
+    school: 'VU Amsterdam',
+    time: '2021 - 2023',
+    degree: 'MSc Neurosciences (Cum Laude)',
+    location: 'Amsterdam, NL',
+    description: 'Internships at Netherlands Institute for Neuroscience (Sleep & Cognition), Concordia University (SCN Lab, Canada), and Leiden UMC.',
+  },
+  {
+    school: 'University of Amsterdam',
+    time: '2017 - 2021',
+    degree: 'BSc Psychobiology',
+    location: 'Amsterdam, NL',
+    description: 'Honours Programme. International exchange at the University of Birmingham (UK). Internship at Amsterdam UMC.',
+  },
 ];
 
-export const skills = [
-	{
-		title: '',
-		description: '',
-	},
-	// {
-	// 	title: 'Experimental Techniques',
-	// 	description: 'Spectroscopy, Isolation of Radioactive Elements, Radiation Measurement',
-	// },
+export const leadershipRoles = [
+  {
+    company: 'Organization for Human Brain Mapping',
+    time: '2024 - present',
+    title: 'Secretary',
+    location: '',
+    description: 'Open Science Special Interest Group.',
+  },
+  {
+    company: 'Dutch Society for Sleep-Wake Research',
+    time: '2023 - present',
+    title: 'Early Career Researchers Committee Member',
+    location: '',
+    description: '',
+  },
+  {
+    company: 'Amsterdam UMC, Dept. of Anatomy and Neurosciences',
+    time: '2023 - present',
+    title: 'Co-chair',
+    location: 'Amsterdam, NL',
+    description: 'Open Science Working Group.',
+  },
+  {
+    company: 'Amsterdam UMC, Dept. of Anatomy and Neurosciences',
+    time: '2023 - 2025',
+    title: 'Chair',
+    location: 'Amsterdam, NL',
+    description: 'Departmental PhD Association.',
+  },
 ];
 
-export const publications = [
-	{
-		title: '',
-		authors: '',
-		journal: '',
-		time: '',
-		link: '',
-		abstract: '',
-	},
-	// {
-	// 	title: 'The Radiation of Uranium Compounds',
-	// 	authors: 'Marie Curie',
-	// 	journal: 'Comptes Rendus de l’Académie des Sciences',
-	// 	time: '1898',
-	// 	link: '#',
-	// 	abstract: 'Early research leading to the identification of uranium’s radioactive properties.',
-	// },
+export const teachingRoles = [
+  {
+    title: 'Public Outreach',
+    description: 'Coordinated hands-on sessions on brain anatomy for primary and high school students; lay-language talks for patients and caregivers; public science events. (2022 - present)',
+  },
+  {
+    title: 'Tutorials, Lectures, and Student Supervision',
+    description: 'Delivered lectures and tutorials across medicine, neuroscience, psychobiology, and nursing programmes, and supervised bachelor\'s and master\'s research projects in clinical and computational neurosciences. (2020 - present)',
+  },
+];
+
+export const grantsAwards = [
+  {
+    company: 'ESMRMB',
+    time: 'Oct 2024',
+    title: 'Best Abstract Award (reproducibility category)',
+    location: '',
+    description: '',
+  },
+  {
+    company: 'ESRS',
+    time: 'Sep 2024',
+    title: 'Sustainable Travel Grant',
+    location: '',
+    description: '€500',
+  },
+  {
+    company: 'ISMRM Benelux',
+    time: 'Mar 2024',
+    title: 'Open Science Challenge',
+    location: '',
+    description: '€500 & registration fee (€125)',
+  },
+  {
+    company: 'ESRS Sleep Science School',
+    time: 'Sep 2023',
+    title: 'Award for Best Oral Presentation (3rd prize)',
+    location: '',
+    description: '',
+  },
+  {
+    company: 'Dutch Society for Sleep-Wake Research',
+    time: 'May 2022',
+    title: 'Young Talent Fund',
+    location: '',
+    description: '€750',
+  },
+  {
+    company: 'Mitacs',
+    time: 'Apr 2022',
+    title: 'Globalink Research Award for Research in Canada',
+    location: '',
+    description: 'CA$6,000',
+  },
+  {
+    company: 'National Conference for Biology Students',
+    time: 'Feb 2022',
+    title: 'Darwin Thesis Award',
+    location: '',
+    description: '',
+  },
+];
+
+export const certificates = [
+  {
+    title: 'MR Operator & MR Safety',
+    description: 'Philips 7T at Spinoza Centre for Neuroimaging; Siemens 3T at Amsterdam UMC, Dept. of Radiology; Philips 7T at Leiden UMC, Dept. of Radiology.',
+  },
+  {
+    title: 'Good Clinical Practice (GCP/WMO)',
+    description: 'Online certification.',
+  },
+  {
+    title: 'Clinical Interviews and Interventions',
+    description: 'Mini-International Neuropsychiatric Interview (MINI); guided online CBT intervention for insomnia "i-Sleep"; motor assessment Parkinson\'s Disease (MDS-UPDRS); neuropsychological evaluation; hallucinations in Parkinson\'s Disease (SAPS-PD).',
+  },
+  {
+    title: 'Teaching Qualifications',
+    description: 'Didactic Training, Teaching and Learning Centre UvA; Didactic Training, Medical Faculty VU Amsterdam.',
+  },
+];
+
+export const programmingSkills = [
+  {
+    title: 'Shell / Bash',
+    description: 'Automation of analysis workflows, large-scale file management.',
+  },
+  {
+    title: 'Python, Matlab, R',
+    description: 'Scientific computing, data processing, visualisation, statistical modelling.',
+  },
+  {
+    title: 'FreeSurfer, FSL, SPM, fMRIPrep',
+    description: 'Structural and functional MRI processing, quality control.',
+  },
+  {
+    title: 'Containerisation (Docker, Apptainer)',
+    description: 'Reproducible, portable environments for multi-site analyses.',
+  },
+  {
+    title: 'GitHub, GitHub Pages',
+    description: 'Version control, collaborative development, building documentation and project websites.',
+  },
 ];
